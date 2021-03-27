de996085bb2da929ed96a5ff4f1696dad8573ce3
skyfast <theskyfast@yahoo.com>
2021-03-26 21:54:08 -0700

    added the page for the points

---
+++ Self Help Gamify/Self Help Gamify/Views/Points.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Self_Help_Gamify.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Points : ContentPage
+    {
+        public Points()
+        {
+            InitializeComponent();
+        }
+    }
+}
